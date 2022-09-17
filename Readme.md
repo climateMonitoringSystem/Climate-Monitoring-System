ee854e7864c4ae693eef26574321535f0024c0ae
offgrid1020 <offgrid1022@gmail.com>
2022-09-17 18:53:30 -0500

    First commit

---
+++ Readme.md
@@ -0,0 +1,25 @@
+# Linux | mac
+
+* python -m venv <venv_name>
+* source <venv_name>/bin/activate
+* pip install --upgrade pip
+* pip install Flask==2.1.2
+* pip install pycryptodome==3.14.1
+* pip install Pyrebase4==4.5.0
+* export FLASK_ENV=development
+* export FLASK_APP=flask_app.py
+* flask run
+
+# Windows
+
+* python -m venv <venv_name>
+* cd <venv_name>/Scripts
+* activate
+* cd ../..
+* pip install --upgrade pip
+* pip install Flask==2.1.2
+* pip install pycryptodome==3.14.1
+* pip install Pyrebase4==4.5.0
+* set FLASK_ENV=development
+* set FLASK_APP=flask_app.py
+* flask run
---
+++ flask_app.py
@@ -0,0 +1,44 @@
+
+
+from flask import (
+    Flask, redirect,
+    url_for, request, jsonify,
+    render_template
+)
+
+
+app = Flask(__name__)
+
+temperature, humidity, light = float(), float(), int()
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return '404 : Not Found'
+
+
+@app.route('/data', methods=['POST', 'GET'])
+def data():
+    global temperature, humidity, light
+    return render_template("index.html", temperature, humidity, light)
+
+
+@app.route('/record', methods=['POST', 'GET'])
+def record():
+    response = {"status": 0}
+    try:
+        global temperature, humidity, light
+        content = request.get_json()
+        temperature = content.get("temperature")
+        humidity = content.get("humidity")
+        light = content.get("light")
+        response["status"] = 1
+
+    except Exception as e:
+        response = {"status": -1}
+
+    return response
+
+
+if __name__ == '__name__':
+    app.run(port=8000)
